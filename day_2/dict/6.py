42af366f9ac606cb966534f3cccaccf2cd520cff
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2022-06-29 03:09:50 -0500

    Delete day_2 directory

--- day_2/dict/6.py
+++
@@ -1,8 +0,0 @@
-avengers = {"ironman":"suit", "captain":"shield", "thor":"hammer"}
-dict_a = {1:"aa", 2:"dd", 4:"ff"}
-
-print(dict_a.items())
-
-
-
-
--- day_2/misc_functions/4.py
+++
@@ -1,7 +0,0 @@
-data = input("enter a value:\n")
-
-print("data is", data)
-print("type of data is", type(data))
-
-data = int(data)
-print("type of data is", type(data))
